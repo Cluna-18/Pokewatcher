d98b0e74a2df3ed1fcd67fe8873d9865cbd7a321
Cristian <lunac@lawrence.edu>
2026-02-22 00:10:06 -0600

    Set base logic to showcase cards

--- Pokewatcher/src/components/CardCard.jsx
+++ Pokewatcher/src/components/CardCard.jsx
@@ -1,8 +1,10 @@
-function CardCard (){
+function CardCard ({name,set,price}){
     return(
-        <>
-        
-        </>
+        <div className = "CardInfo">
+            <p>Name: {name}</p>
+            <p>Pokemon set: {set}</p>
+            <p>Current price: ${price}</p>
+        </div>
     )
 }
 
--- Pokewatcher/src/pages/Home.jsx
+++ Pokewatcher/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import CardCard from "../components/CardCard.jsx"
+
 function Home () {
 
     return(
         <div className = "homePage">
-            <h1>Welcome and enjoy tracking card prices!</h1>
 
+            <h1>Welcome and enjoy tracking card prices!</h1>
+            <CardCard name={"Pikachu"} set={"Base Set"} price={15.99}/>
+            <CardCard name={"Charizard"} set={"Base Set"} price={25.99}/>
+            <CardCard name={"Blastoise"} set={"Base Set"} price={20.99}/>
         </div>
     )
 }
